4ae9337bca0f8cbcbd834a93b8212b3d18460380
0x330a <92654767+0x330a@users.noreply.github.com>
2024-12-24 08:50:04 +1100

    feat: vite config for ssr noexternal

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
+	ssr: {
+		noExternal: ["@farcaster/frame-sdk", "@farcaster/frame-core", "@farcaster/frame-wagmi-connector"]
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
