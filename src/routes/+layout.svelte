03ee2efbeae244b1a49f9547fe3f88001ec71069
0x330a <92654767+0x330a@users.noreply.github.com>
2024-12-24 14:21:04 +1100

    refactor: try re-arrange

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	// frames
 	import { onMount } from "svelte";
 	import { defaultConfig } from "svelte-wagmi";
-	import farcasterFrame from "@farcaster/frame-wagmi-connector";
+	import {farcasterFrame} from "@farcaster/frame-wagmi-connector";
 
 	import { optimism } from 'viem/chains';
 	import { PUBLIC_WALLET_CONNECT_PROJECT_ID } from '$env/static/public';
@@ -13,21 +13,18 @@
 	import { injected, walletConnect } from '@wagmi/connectors';
 
 	onMount(async () => {
-		await sdk.actions.ready();
 		const easyAgent = defaultConfig({
 			appName: "easy-agent",
 			chains: [optimism],
 			walletConnectProjectId: PUBLIC_WALLET_CONNECT_PROJECT_ID,
-			connectors: [injected(), farcasterFrame(), walletConnect({
+			connectors: [farcasterFrame(), injected(), walletConnect({
 				showQrModal: false,
 				projectId: PUBLIC_WALLET_CONNECT_PROJECT_ID
 			})],
 		})
 		await easyAgent.init();
+		await sdk.actions.ready();
 	});
-
-
-
 
 </script>
 
